69913e4fee589b71f6e81d15bae8abee87a43548
ahadsenja <hamdanilm96@gmail.com>
2019-04-01 21:01:38 +0800

    feat: add new todo list

---
+++ src/component/TodoApp/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = props => (
+  <ul>
+    {
+      props.todos.map((todo, index) => <li key={index}>{todo}</li>)
+    }
+  </ul>
+);
+
+export default List;
